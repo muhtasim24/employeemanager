928684db4801980df995c5e35d23a236b640123f
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-11-22 23:19:22 -0500

    Finished Service class

---
+++ employeemanager/src/main/java/tech/mmushfiq/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package tech.mmushfiq.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+        // going to call the construcotr on the class and take the passed down message
+
+    }
+    
+}
