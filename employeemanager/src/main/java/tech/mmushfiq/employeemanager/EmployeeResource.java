6a4185579ef3e3f3afc90ee621631dc75f2c6ed8
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-11-25 12:59:15 -0500

    Finished Service, began working on controller

--- employeemanager/src/main/java/tech/mmushfiq/employeemanager/EmployeeResource.java
+++ employeemanager/src/main/java/tech/mmushfiq/employeemanager/EmployeeResource.java
@@ -1,5 +1,44 @@
 package tech.mmushfiq.employeemanager;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tech.mmushfiq.employeemanager.model.Employee;
+import tech.mmushfiq.employeemanager.repo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeResource {
-    
+    //bringing in the service
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // method to return all of the employees in the application, going to return a HTTP Response
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        // just need to call the service and have the service return the list 
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    // method to find a user by the id after getting to path employee
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) { // id from path
+        // just need to call the service and have the service return the list 
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    //method to add an employee
+    // going to need a post request
 }
