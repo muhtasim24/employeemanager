2ce55953709baa0f68e5a023c6161a4a1fe873de
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-11-25 15:51:23 -0500

    Created Angular Services of basic functions on the app of adding, getting, updating, deleting employees

---
+++ employeemanagerapp/src/app/employee.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Employee } from "./employee";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployeeService {
+    private apiServerUrl = '';
+
+    constructor(private http: HttpClient) {}
+    
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+      }
+
+    //returning only 1 employee and adding an employee
+    public addEmployee(employee: Employee): Observable<Employee> {
+        // This is a post request to the url
+        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+    }
+
+    // update an employee
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+    }
+
+    // whenever we delete an employee we dont send anything back, we just send back HTTP status so its returning void
+    public deleteEmployee(employeeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+    }
+}
---
+++ employeemanagerapp/src/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    // defining all the attributes an employee is going to have 
+    id: number;
+    name: string;
+    email: string;
+    jobTitle: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+}
