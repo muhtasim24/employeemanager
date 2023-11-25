89b7fd9aee30fbddc06605292b552616bfb09740
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-11-25 13:39:23 -0500

    Created Rest Controller, add, find, update, delete functions

--- employeemanager/src/main/java/tech/mmushfiq/employeemanager/EmployeeResource.java
+++ employeemanager/src/main/java/tech/mmushfiq/employeemanager/EmployeeResource.java
@@ -2,11 +2,15 @@ package tech.mmushfiq.employeemanager;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,4 +45,23 @@ public class EmployeeResource {
 
     //method to add an employee
     // going to need a post request
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    // method to update an employee, only difference from adding is we are given the id
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
+
+    //method ot delete an employee, delete by id
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
