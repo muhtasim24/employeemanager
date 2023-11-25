2891b6232c961108091f209d7d428e3ee502ff16
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-11-25 16:19:31 -0500

    Fixed up and finished Angular Services

--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,33 +1,37 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Employee } from "./employee";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class EmployeeService {
-    private apiServerUrl = '';
+  private apiServerUrl = '';
 
-    constructor(private http: HttpClient) {}
-    
-    public getEmployees(): Observable<Employee[]> {
-        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
-      }
+  constructor(private http: HttpClient) { }
 
-    //returning only 1 employee and adding an employee
-    public addEmployee(employee: Employee): Observable<Employee> {
-        // This is a post request to the url
-        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
-    }
+  // needs a get request to get the employees
+  // tells the http client where to make the request 
+  // this is going to be a get request
+  // this is typescript so we have to put in the type we are expecting
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`)
+  }
 
-    // update an employee
-    public updateEmployee(employee: Employee): Observable<Employee> {
-        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
-    }
+  //returning only 1 employee and adding an employee
+  public addEmployee(employee: Employee): Observable<Employee> {
+    // This is a post request to the url
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
 
-    // whenever we delete an employee we dont send anything back, we just send back HTTP status so its returning void
-    public deleteEmployee(employeeId: number): Observable<void> {
-        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
-    }
+  // update an employee
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  // whenever we delete an employee we dont send anything back, we just send back HTTP status so its returning void
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
 }
