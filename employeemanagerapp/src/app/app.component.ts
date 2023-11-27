8918e4509c4fb4a7971f18a54fecd2dc9470a45f
Muhtasim Mushfiq <muhtasim01@gmail.com>
2023-11-27 01:31:15 -0500

    finished up Angular Component section

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { response } from 'express';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +13,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+// once we implement on init 
+export class AppComponent implements OnInit{
   title = 'employeemanagerapp';
+
+  // this is going to hold all the employees coming from the backend
+  public employees!: Employee[];
+
+  constructor(private employeeSerivce: EmployeeService) {}
+
+  // this is going to run everytime this component is going to be called and call our getEmployees function
+  ngOnInit(): void {
+      this.getEmployees();
+  }
+  public getEmployees(): void {
+    this.employeeSerivce.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      }, (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+    // this is an observabe making a request over the internet, we need to call subscribe so we can be notified when data comes back from the server or an error
+  }
+
 }
--- employeemanagerapp/src/app/employee.service.ts
+++ employeemanagerapp/src/app/employee.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
